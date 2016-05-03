fcc5cbd2c25044ec311c368fc138f7b2e0d871d8
Arun <Arun@ln-mowifi-05-10.1.53.191.net.bton.ac.uk>
2016-05-03 13:24:54 +0100

    added missing files

---
+++ src/BBox.h
@@ -0,0 +1,25 @@
+#ifndef BBox_H
+#define BBox_H
+
+
+class BBox
+{
+	
+private:
+std::shared_ptr<glm::	
+	
+	
+	
+	
+public:	
+
+BBox();
+virtual ~ BBox();
+
+void translate(glm::vec3);
+	
+	
+};
+
+
+#endif
