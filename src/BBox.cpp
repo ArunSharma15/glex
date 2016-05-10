07f5ff68d61b98e69a5391160d55e065b0a3ff2b
Arun <Arun@ln-mowifi-02-10.1.26.26.net.bton.ac.uk>
2016-05-10 17:57:20 +0100

    created prototype methods

--- src/BBox.cpp
+++ src/BBox.cpp
@@ -1,10 +1,28 @@
 #include <glm/glm.hpp>
 #include "BBox.h"
 
-Bbox::Bbox()
+Bbox::Bbox(					glm::vec3 x_y_z_pos,
+							glm::vec3 translate, 
+                            glm::vec3 animate, 
+                            glm::vec3 rotate,
+                            glm::vec3 scale)
 {
+	Bbox::translate(glm::vec3 translate){
+		
+		scale = scale +  glm::vec3(0.5f,0.5f,0.1f);
+	}
 	
+	Bbox::scale(glm::vec3 scale){
 	
+	scale = scale +  glm::vec3(0.5f,0.5f,0.1f);
+		
+	}	
+	
+	Bboxx::pos(glm::vec3 x_y_z_pos){
+	
+	x_y_z_pos = x_y_z_pos +  glm::vec3(0.5f,0.5f,0.1f);
+		
+	}
 	
 	
 	
--- src/BBox.h
+++ src/BBox.h
@@ -6,14 +6,16 @@ class BBox
 {
 	
 private:
-std::shared_ptr<glm::	
-	
-	
-	
 	
 public:	
 
-BBox();
+
+
+BBox(						glm::vec3 x_y_z_pos, 
+							glm::vec3 translate, 
+                            glm::vec3 animate, 
+                            glm::vec3 rotate,
+                            glm::vec3 scale);
 virtual ~ BBox();
 
 void translate(glm::vec3);
