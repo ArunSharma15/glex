edf0d80cae3685c32a6f7edcbc7c7235b04a4069
Arun <Arun@ln-mowifi-05-10.1.49.77.net.bton.ac.uk>
2016-05-10 15:43:00 +0100

    amended python file

--- src/BBox.cpp
+++ src/BBox.cpp
@@ -1,16 +1,15 @@
 #include <glm/glm.hpp>
 #include "BBox.h"
 
-int main()
+Bbox::Bbox()
 {
 	
-glm::mat4 model_matrix(1.0);
-glm::vec3 t(1.0,1.0,1.0);
-Bbox 1();
-b1.translate();
-b1.scale(1.0,2.0,3.0);
-b1.rotateX(0.25);
-b1.rotateY(0.25);
-b1.rotateZ(0.25);
+	
+	
+	
+	
 	
 }
+
+
+
--- src/test.py
+++ src/test.py
@@ -1,3 +1,3 @@
 import libhydra
 
-bindtest
+libhydra.bindtest
